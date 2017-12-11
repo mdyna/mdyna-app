3bcf38ded13ee54ab85aa8b7c0f50e72b164672a
David Morais <davidmorais92@gmail.com>
2017-12-11 00:13:39 +0000

    Add header component and add task button

--- src/components/App.js
+++ src/components/App.js
@@ -4,23 +4,51 @@ import logo from '../../assets/DinagramLogo.png';
 import TEST_DATA from '../dev/testData.json';
 import Button from 'grommet/components/Button';
 import App from 'grommet/components/App';
+import Article from 'grommet/components/Article';
+import Section from 'grommet/components/Section';
+import Header from 'grommet/components/Header';
+import Heading from 'grommet/components/Heading';
+import Image from 'grommet/components/Image';
+import Pulse from 'grommet/components/icons/Pulse';
 
-import "!style-loader!css-loader!sass-loader!../../node_modules/grommet/grommet-hpe.min.css";
+import "!style-loader!css-loader!sass-loader!../../node_modules/grommet/grommet-hpe.min.css"; 
 
 class Dinagram extends Component {
   render() {
     return (
-      <App>
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h2>Tsts</h2>
-        </div>
-        <p className="App-intro">
-          To get gtarted, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <App className="dinagram-app">
+        <Article>
+          <Header
+            size="small"
+          >
+            <Image
+              src={logo}
+              className="App-logo"
+              alt="dinagram logo"
+              size="medium"
+            />
+            <Heading
+              size="small"
+              align="center"
+              strong
+              tag="h1"
+            >
+              Making the most of your free time
+            </Heading>
+          </Header>
+        <Section
+          align="center"
+          
+        >
+          <Button
+            className="add-task-btn">
+            <Pulse />
+          </Button>
+        </Section>
+      </Article>
       </App>
     );
   }
-}
+} 
 
 export default Dinagram;
