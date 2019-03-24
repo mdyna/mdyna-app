8d7078d671745f8cd1ba83187434b75aeb6bb656
David Morais <davidmorais92@gmail.com>
2019-03-24 17:40:43 +0000

    Create onHover logic for tooltip component   - Wrap component in a div so it detects hover on teh whole thing and not only on the svg

--- app/components/Tooltip.jsx
+++ app/components/Tooltip.jsx
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import NoteItem from './Cards/CardItem';
+import HelpIcon from 'grommet/components/icons/base/Help';
+import Box from 'grommet/components/Box';
+import CardItem from './Cards/CardItem';
 
 class Tooltip extends Component {
+  state = {
+    show: false,
+  };
+
+  showTooltip() {
+    this.setState({ show: true });
+  }
+
+  hideTooltip() {
+    this.setState({ show: false });
+  }
+
   render() {
-    const { text, color = '#a7ffeb', title = 'Help' } = this.props;
-    return <NoteItem className="tooltip" card={{ text, color, title }} showAllText />;
+    const { text, color, title} = this.props;
+    console.log('rendering', text)
+
+    return (
+      <Box onMouseEnter={() => this.showTooltip()} onMouseLeave={() => this.hideTooltip()} className="tip-icon">
+        <HelpIcon />
+        {this.state.show ? (
+          <CardItem className="tooltip" card={{ text, color, title }} showAllText />
+        ) : (
+          ''
+        )}
+      </Box>
+    );
   }
 }
 
@@ -13,11 +38,12 @@ export default Tooltip;
 
 Tooltip.propTypes = {
   text: PropTypes.string.isRequired,
-  color: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  show: PropTypes.bool,
+  color: PropTypes.string,
+  title: PropTypes.string,
 };
 
 Tooltip.defaultProps = {
   show: false,
+  title: 'help',
+  color: '#a7ffeb',
 };
