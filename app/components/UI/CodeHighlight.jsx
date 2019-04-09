5592f55ad8a9b5a4f9283226e63fbed683308457
David Morais <davidmorais92@gmail.com>
2019-04-09 19:01:12 +0100

    Fix tabs in cards

--- app/components/UI/CodeHighlight.jsx
+++ app/components/UI/CodeHighlight.jsx
@@ -39,7 +39,10 @@ class Highlight extends React.Component {
     const codeBlocks = this.state.codeBlocks;
     for (let i = 0; i < nodes.length; i += 1) {
       nodes[i].textContent = (codeBlocks && codeBlocks[i]) || nodes[i].textContent.trim();
-      hljs.highlightBlock(nodes[i]);
+      hljs.configure({
+        tabReplace: '  ',
+      })
+      hljs.highlightBlock(nodes[i])
     }
   }
 
