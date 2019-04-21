8bbebb4be248c148dcb885eb8fdaca0061fde6e1
David Morais <davidmorais92@gmail.com>
2019-04-21 23:30:48 +0100

    Update task list items design Close #PM169

--- app/components/UI/TaskListInput.jsx
+++ app/components/UI/TaskListInput.jsx
@@ -7,32 +7,34 @@ class TaskListInput extends Component {
     const { text, editCard, defaultChecked } = this.props;
 
     return (
-      <input
-        className="card-tasklist"
-        type="checkbox"
-        checked={defaultChecked === 'true'}
-        onChange={(e) => {
-          e.stopPropagation();
-          const { card, saveFunc } = editCard;
-          const inputTextContent = e.target.parentElement.textContent;
-          let newText = '';
-          if (e.target.checked) {
-            const inputText = `[ ]${inputTextContent}`;
-            newText = text.replace(inputText, `[X]${inputTextContent}`);
-          } else {
-            const inputText =
-              (text.indexOf(`[X]${inputTextContent}`) !== -1 && `[X]${inputTextContent}`) ||
-              (text.indexOf(`[x]${inputTextContent}`) !== -1 && `[x]${inputTextContent}`) ||
-              null;
-            newText = text.replace(inputText, `[ ] ${inputTextContent}`);
-          }
+      <React.Fragment>
+        <input
+          className="card-tasklist"
+          type="checkbox"
+          checked={defaultChecked === 'true'}
+          onChange={(e) => {
+            e.stopPropagation();
+            const { card, saveFunc } = editCard;
+            const inputTextContent = e.target.parentElement.textContent;
+            let newText = '';
+            if (e.target.checked) {
+              const inputText = `[ ]${inputTextContent}`;
+              newText = text.replace(inputText, `[X]${inputTextContent}`);
+            } else {
+              const inputText =
+                (text.indexOf(`[X]${inputTextContent}`) !== -1 && `[X]${inputTextContent}`) ||
+                (text.indexOf(`[x]${inputTextContent}`) !== -1 && `[x]${inputTextContent}`) ||
+                null;
+              newText = text.replace(inputText, `[ ] ${inputTextContent}`);
+            }
 
-          saveFunc({
-            ...card,
-            text: newText,
-          });
-        }}
-      />
+            saveFunc({
+              ...card,
+              text: newText,
+            });
+          }}
+        />
+      </React.Fragment>
     );
   }
 }
