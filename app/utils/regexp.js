92a02b1773da3ca55f83112b40bb8a5485b24bbe
David Morais <davidmorais92@gmail.com>
2019-04-23 10:56:16 +0100

    Fix regexp for taskslists in markdown

--- app/utils/regexp.js
+++ app/utils/regexp.js
@@ -1,6 +1,6 @@
 const codeRegExp = new RegExp(/`{3}[\s\S]*?`{3}/g);
 const backticksRegExp = new RegExp(/(?:```[a-z]*)/g);
-const extractTasksFromMarkdownRegExp = new RegExp(/^(- [\[( |X)\] (\w\d~%:`#*?&)]*)/gm);
+const extractTasksFromMarkdownRegExp = new RegExp(/^(- \[( |X)\]([^\n]+)*)/gm);
 
 export default {
   codeRegExp,
