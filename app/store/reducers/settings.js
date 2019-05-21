73c9f9a2859a936340def30ad1d1a651a28635f4
David Morais <davidmorais92@gmail.com>
2019-05-21 23:37:40 +0100

    Add actions and reducers for settings

---
+++ app/store/reducers/settings.js
@@ -0,0 +1,23 @@
+import ACTION_TYPES from '../actions/actionTypes';
+
+const cwd = window.cwd || '';
+
+const { CHANGE_CWD } = ACTION_TYPES;
+
+function changeCwd(
+  state = {
+    cwd,
+  },
+  action,
+) {
+  switch (action.type) {
+    case CHANGE_CWD:
+      return {
+        cwd: action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+export default changeCwd;
