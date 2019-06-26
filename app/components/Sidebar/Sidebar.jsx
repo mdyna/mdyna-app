712400b4ffc560afee57acdf925bf7bca655d98b
David Morais <davidmorais92@gmail.com>
2019-06-26 23:57:15 +0100

    Refactor sidebar   - Use collapsible component to render the sidebar   - Render both collapsed and non-collapsed items to use translations   - Remove extranous CSS and re-do the stylesheet for the sidebar component.   - Adjust button styles and remove important rules from normalize-button-icons - Fix app layout, remove grid and extra box - Fix overflow-y of root component - Add temporary styling to leave hover and active states consistent with each other - Fix width for card list - Do required adjustments in FolderPicker, LabelFilter and Tooltip components

--- app/components/Sidebar/Sidebar.jsx
+++ app/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 // eslint-disable-next-line
-import { ipcRenderer } from 'electron';
+import { ipcRenderer } from "electron";
 import PropTypes from 'prop-types';
-import { Box, Text } from 'grommet';
+import { Box, Text, Collapsible } from 'grommet';
 import {
   Filter,
   Brush,
@@ -30,7 +30,7 @@ import {
 } from 'Utils/globals';
 
 import SearchInput from './Search';
-import './Sidebar.scss'; // eslint-disable-line
+import "./Sidebar.scss"; // eslint-disable-line
 
 function getCardTitles(cards) {
   return (cards && cards.map(d => d && d.title)) || '';
@@ -43,9 +43,17 @@ class Sidebar extends Component {
 
   searchBar = React.createRef();
 
+  getSortingOrder = (targetSorting) => {
+    const { sorting, order } = this.props;
+    const activeSorting = sorting;
+    if (targetSorting === activeSorting) {
+      return order === ASCENDING_ORDER ? DESCENDING_ORDER : ASCENDING_ORDER;
+    }
+    return ASCENDING_ORDER;
+  };
+
   expandMenu() {
     const { toggleSidebar } = this.props;
-
     toggleSidebar();
   }
 
@@ -65,7 +73,8 @@ class Sidebar extends Component {
     searchCards(value);
   }
 
-  render() {
+
+  collapsibleSidebar() {
     const {
       cards,
       whiteMode,
@@ -75,7 +84,6 @@ class Sidebar extends Component {
       cwd,
       removeLabelFilter,
       sidebarExpanded,
-      toggleSidebar,
       toggleWhiteMode,
       toggleEditor,
       toggleCompletedFilter,
@@ -89,247 +97,113 @@ class Sidebar extends Component {
     const labelFilterFuncs = { addLabelFilter, removeLabelFilter };
     const titles = [...getCardTitles(cards)];
 
-    const getSortingOrder = (targetSorting) => {
-      const activeSorting = sorting;
-      if (targetSorting === activeSorting) {
-        return order === ASCENDING_ORDER ? DESCENDING_ORDER : ASCENDING_ORDER;
-      }
-      return ASCENDING_ORDER;
-    };
-
     return (
-      <Box
-        full="vertical"
-        justify="start"
-        className={classnames('sidebar', {
-          'white-mode': whiteMode,
-          collapsed: !sidebarExpanded,
-        })}
-        pad="small"
-        direction="column"
-      >
-        <KeyboardEventHandler
-          handleKeys={['ctrl+p']}
-          onKeyEvent={() => {
-            if (!sidebarExpanded) {
-              this.expandMenu();
-            }
-            setTimeout(() => this.searchBar.current.componentRef.focus(), 300);
-          }}
-        />
-        <Box className="menu-item title">
-          {sidebarExpanded ? (
-            <Button onClick={() => toggleSidebar()} className="title-button">
-              <FormPrevious />
-              <Text size="large">mdyna</Text>
-            </Button>
-          ) : (
-            <Button onClick={() => toggleSidebar()} className="title-button">
-              <FormNext />
-            </Button>
-          )}
-        </Box>
-        <Box direction="row" justify="start" className="menu-item">
-          {sidebarExpanded ? (
-            <SearchInput
-              titles={titles}
-              whiteMode={whiteMode}
-              onChange={e => this.changeSearchInput(e)}
-              searchBar={this.searchBar}
-              searchInput={searchInput}
-            />
-          ) : (
-            <Tooltip
-              whiteMode={whiteMode}
-              icon={<Search />}
-              title="Search"
-              className="sidebar-tooltip"
-              text="Hotkey: Ctrl+P"
-              onClick={() => {
-                this.expandMenu();
-                setTimeout(() => this.searchBar.current.componentRef.focus(), 300);
-              }}
-            />
-          )}
-        </Box>
-
-        <Box direction="row" justify="start" className="menu-item">
-          {sidebarExpanded ? (
-            <Button
-              onClick={() => {
-                toggleWhiteMode(!whiteMode);
-              }}
-            >
-              <Brush />
-              <Text className="menu-label">{whiteMode ? 'Dark Theme' : 'Light Theme'}</Text>
-            </Button>
-          ) : (
-            <Tooltip
-              whiteMode={whiteMode}
-              icon={<Brush />}
-              className="sidebar-tooltip"
-              title="Set theme"
-              text={`Switch to ${whiteMode ? 'dark' : 'white'} theme`}
-              onClick={() => {
-                toggleWhiteMode(!whiteMode);
-              }}
-            />
-          )}
-        </Box>
-        <Box direction="row" justify="start" className="menu-item">
-          {sidebarExpanded ? (
-            <Button
-              onClick={() => {
-                toggleEditor(true);
-              }}
-              className="add-note-btn"
-            >
-              <AddCircle />
-              <Text className="menu-label">Add Card</Text>
-            </Button>
-          ) : (
-            <Tooltip
-              whiteMode={whiteMode}
-              icon={<AddCircle />}
-              className={classnames('sidebar-tooltip', 'add-note-btn')}
-              title="Add card"
-              text="Hotkey: A"
-              onClick={() => {
-                toggleEditor(true);
-              }}
-            />
-          )}
-        </Box>
-        <Box direction="row" justify="start" className="menu-item">
-          {sidebarExpanded ? (
-            <Button
-              onClick={() => {
-                toggleCompletedFilter(!completedFilterOn);
-              }}
-              className={classnames('toggle-completed-button', completedFilterOn && 'active')}
-            >
-              <Checkmark />
-              <Text className="menu-label">Toggle Completed</Text>
-            </Button>
-          ) : (
-            <Tooltip
-              className={classnames(
-                'toggle-completed-button',
-                'sidebar-tooltip',
-                completedFilterOn && 'active',
-              )}
-              whiteMode={whiteMode}
-              icon={<Checkmark />}
-              title="Toggle completed cards"
-              text="Make cards which have already been completed visible"
-              onClick={() => {
-                toggleCompletedFilter(!completedFilterOn);
-              }}
-            />
-          )}
-        </Box>
-        <Box direction="row" justify="start" className="menu-item">
-          {sidebarExpanded ? (
-            <Button
-              onClick={() => {
-                this.expandSortingOptions();
-              }}
-            >
-              <Sort className="sort-icon" />
-              <Text className="menu-label">Sort Cards </Text>
-            </Button>
-          ) : (
-            <Tooltip
-              className={classnames('sidebar-tooltip', 'sort-icon')}
-              whiteMode={whiteMode}
-              icon={<Sort />}
-              title="Sort cards"
-              text="Open sorting options"
-              onClick={() => {
-                this.expandMenu();
-                setTimeout(() => this.expandSortingOptions(), 300);
-              }}
-            />
-          )}
-        </Box>
-        {sidebarExpanded && (
-          <Box
-            direction="column"
-            className={classnames(sortingOptionsExpanded && 'expanded', 'sorting-table')}
+      <Collapsible direction="horizontal" open={sidebarExpanded}>
+        <Box direction="column" align="end">
+          <SearchInput
+            titles={titles}
+            whiteMode={whiteMode}
+            onChange={e => this.changeSearchInput(e)}
+            searchBar={this.searchBar}
+            searchInput={searchInput}
+          />
+          <Button
+            onClick={() => {
+              toggleWhiteMode(!whiteMode);
+            }}
           >
+            <Brush />
+            <Text className="menu-label">
+              {whiteMode ? 'Dark Theme' : 'Light Theme'}
+            </Text>
+          </Button>
+          <Button
+            onClick={() => {
+              toggleEditor(true);
+            }}
+            className="add-note-btn"
+          >
+            <AddCircle />
+            <Text className="menu-label">Add Card</Text>
+          </Button>
+          <Button
+            onClick={() => {
+              toggleCompletedFilter(!completedFilterOn);
+            }}
+            className={classnames(
+              'toggle-completed-button',
+              completedFilterOn && 'active',
+            )}
+          >
+            <Checkmark />
+            <Text className="menu-label">Toggle Completed</Text>
+          </Button>
+          <Button
+            onClick={() => {
+              this.expandSortingOptions();
+            }}
+          >
+            <Sort className="sort-icon" />
+            <Text className="menu-label">Sort Cards </Text>
+          </Button>
+          <Collapsible direction="vertical" open={sortingOptionsExpanded}>
             <Button
-              className={classnames(sorting === SORTING_BY_TITLE && 'active-sorting')}
-              onClick={() => changeSorting(SORTING_BY_TITLE, getSortingOrder(SORTING_BY_TITLE))}
+              className={classnames(
+                sorting === SORTING_BY_TITLE && 'active',
+              )}
+              onClick={() => changeSorting(
+                SORTING_BY_TITLE,
+                this.getSortingOrder(SORTING_BY_TITLE),
+              )
+              }
             >
-              <Up className={classnames(order === DESCENDING_ORDER && 'descending')} />
+              <Up
+                className={classnames(
+                  order === DESCENDING_ORDER && 'descending',
+                )}
+              />
               By Title
             </Button>
             <Button
-              onClick={() => changeSorting(SORTING_BY_DATE, getSortingOrder(SORTING_BY_DATE))}
-              className={classnames(sorting === SORTING_BY_DATE && 'active-sorting')}
+              onClick={() => changeSorting(
+                SORTING_BY_DATE,
+                this.getSortingOrder(SORTING_BY_DATE),
+              )
+              }
+              className={classnames(
+                sorting === SORTING_BY_DATE && 'active',
+              )}
             >
-              <Up className={classnames(order === DESCENDING_ORDER && 'descending')} />
+              <Up
+                className={classnames(
+                  order === DESCENDING_ORDER && 'descending',
+                )}
+              />
               By Date
             </Button>
-          </Box>
-        )}
-
-        <Box direction="column" className="menu-item-labels">
-          {(sidebarExpanded && (
-            <FolderPicker
-              label="Change directory"
-              placeholder={cwd}
-              whiteMode={whiteMode}
-              onChange={(value) => {
-                changeCwd(value);
-                ipcRenderer.send('CHANGED-CWD');
-              }}
-            />
-          )) || (
-            <Tooltip
-              className={classnames('sidebar-tooltip', 'sort-icon')}
-              whiteMode={whiteMode}
-              icon={<FolderCycle />}
-              title="Change Cards Directory"
-              text="Change the directory in which your cards live. If you connect it to Dropbox or Google Drive, you can have your cards in multiple devices"
-              onClick={() => {
-                this.expandMenu();
-              }}
-            />
-          )}
-          <Box direction="row" justify="start" className="menu-item">
-            {sidebarExpanded ? (
-              <React.Fragment>
-                <Filter />
-                <Text className="menu-label-filter">Filter Labels</Text>
-              </React.Fragment>
-            ) : (
-              <Tooltip
-                className="sidebar-tooltip"
-                whiteMode={whiteMode}
-                icon={<Filter />}
-                title="Filter cards"
-                text="Filter cards by label"
-                onClick={() => {
-                  this.expandMenu();
-                }}
-              />
-            )}
-          </Box>
-          {sidebarExpanded ? (
+          </Collapsible>
+          <FolderPicker
+            label="Change directory"
+            placeholder={cwd}
+            className="menu-label"
+            whiteMode={whiteMode}
+            onChange={(value) => {
+              changeCwd(value);
+              ipcRenderer.send('CHANGED-CWD');
+            }}
+          />
+          <Box direction="column" className="menu-label-filter">
+            <Filter />
+            <Text className="menu-label">Filter Labels</Text>
             <LabelFilter
               whiteMode={whiteMode}
               labels={labels}
               labelFilters={labelFilters}
               labelFilterFuncs={labelFilterFuncs}
             />
-          ) : (
-            ''
-          )}
-        </Box>
-        {sidebarExpanded && (
-          <Box className="sidebar-footer">
-            <Text size="small">
+          </Box>
+          <Box direction="column">
+            <Text size="small" className="help">
               Markdown Guide
               <Tooltip
                 whiteMode={whiteMode}
@@ -337,7 +211,7 @@ class Sidebar extends Component {
                 title={TooltipData.markdown.title}
               />
             </Text>
-            <Text size="small">
+            <Text size="small" className="help">
               Keyboard Shortcuts
               <Tooltip
                 whiteMode={whiteMode}
@@ -347,8 +221,133 @@ class Sidebar extends Component {
             </Text>
             <Text size="small">{window.appVersion}</Text>
           </Box>
-        )}
-      </Box>
+        </Box>
+      </Collapsible>
+    );
+  }
+
+  render() {
+    const {
+      whiteMode,
+      sidebarExpanded,
+      toggleSidebar,
+      toggleWhiteMode,
+      toggleEditor,
+      toggleCompletedFilter,
+      completedFilterOn,
+    } = this.props;
+
+    return (
+      <React.Fragment>
+
+        <KeyboardEventHandler
+          handleKeys={['ctrl+p']}
+          onKeyEvent={() => {
+            if (!sidebarExpanded) {
+              this.expandMenu();
+            }
+            setTimeout(() => this.searchBar.current.componentRef.focus(), 300);
+          }}
+        />
+        <Box
+          className={classnames(whiteMode && 'white-mode', 'sidebar', sidebarExpanded && 'expanded')}
+          direction="column"
+          alignContent="end"
+        >
+          <Box direction="row" className="menu-controller">
+            {sidebarExpanded ? (
+              <Button onClick={() => toggleSidebar()} className="title-button">
+                <FormPrevious />
+                <Text size="large">mdyna</Text>
+              </Button>
+            ) : (
+              <Button onClick={() => toggleSidebar()} className="title-button">
+                <FormNext />
+              </Button>
+            )}
+          </Box>
+          <Tooltip
+            whiteMode={whiteMode}
+            icon={<Search />}
+            title="Search"
+            className="sidebar-tooltip"
+            text="Hotkey: Ctrl+P"
+            onClick={() => {
+              this.expandMenu();
+              setTimeout(
+                () => this.searchBar.current.componentRef.focus(),
+                300,
+              );
+            }}
+          />
+          <Tooltip
+            className="sidebar-tooltip"
+            whiteMode={whiteMode}
+            icon={<Filter />}
+            title="Filter cards"
+            text="Filter cards by label"
+            onClick={() => {
+              this.expandMenu();
+            }}
+          />
+          <Tooltip
+            whiteMode={whiteMode}
+            icon={<Brush />}
+            className="sidebar-tooltip"
+            title="Set theme"
+            text={`Switch to ${whiteMode ? 'dark' : 'white'} theme`}
+            onClick={() => {
+              toggleWhiteMode(!whiteMode);
+            }}
+          />
+          <Tooltip
+            whiteMode={whiteMode}
+            icon={<AddCircle />}
+            className={classnames('sidebar-tooltip', 'add-note-btn')}
+            title="Add card"
+            text="Hotkey: A"
+            onClick={() => {
+              toggleEditor(true);
+            }}
+          />
+          <Tooltip
+            className={classnames(
+              'toggle-completed-button',
+              'sidebar-tooltip',
+              completedFilterOn && 'active',
+            )}
+            whiteMode={whiteMode}
+            icon={<Checkmark />}
+            title="Toggle completed cards"
+            text="Make cards which have already been completed visible"
+            onClick={() => {
+              toggleCompletedFilter(!completedFilterOn);
+            }}
+          />
+          <Tooltip
+            className={classnames('sidebar-tooltip', 'sort-icon')}
+            whiteMode={whiteMode}
+            icon={<Sort />}
+            title="Sort cards"
+            text="Open sorting options"
+            onClick={() => {
+              this.expandMenu();
+              setTimeout(() => this.expandSortingOptions(), 300);
+            }}
+          />
+          <Tooltip
+            className={classnames('sidebar-tooltip', 'sort-icon')}
+            whiteMode={whiteMode}
+            icon={<FolderCycle />}
+            title="Change Cards Directory"
+            text="Change the directory in which your cards live. If you connect it to Dropbox or Google Drive, you can have your cards in multiple devices"
+            onClick={() => {
+              this.expandMenu();
+            }}
+          />
+          {this.collapsibleSidebar()}
+        </Box>
+      </React.Fragment>
     );
   }
 }
--- app/components/UI/FolderPicker.jsx
+++ app/components/UI/FolderPicker.jsx
@@ -1,6 +1,6 @@
 import React, { PureComponent, Fragment } from 'react';
 import cx from 'classnames';
-import {FolderCycle} from 'grommet-icons';
+import { FolderCycle } from 'grommet-icons';
 import PropTypes from 'prop-types';
 
 import './FolderPicker.scss';
@@ -8,11 +8,11 @@ import './FolderPicker.scss';
 export default class FolderPicker extends PureComponent {
   render() {
     const {
-      onChange, label, value, whiteMode, placeholder,
+      onChange, label, value, whiteMode, placeholder, className,
     } = this.props;
     return (
       <Fragment>
-        <label className={cx('picker', whiteMode && 'white-mode')} htmlFor="folder-picker">
+        <label className={cx('picker', whiteMode && 'white-mode', className)} htmlFor="folder-picker">
           <FolderCycle />
           {label}
           <input
@@ -20,7 +20,7 @@ export default class FolderPicker extends PureComponent {
             type="file"
             webkitdirectory=""
             style={{ visibility: 'none' }}
-            onChange={e => {onChange(e.target.files[0].path)}}
+            onChange={e => onChange(e.target.files[0].path)}
             placeholder={value}
           />
         </label>
@@ -36,9 +36,11 @@ FolderPicker.propTypes = {
   placeholder: PropTypes.string,
   onChange: PropTypes.func.isRequired,
   value: PropTypes.string,
+  className: PropTypes.string,
 };
 
 FolderPicker.defaultProps = {
+  className: '',
   value: '',
   whiteMode: false,
   label: '',
