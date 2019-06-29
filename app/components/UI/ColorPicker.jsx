eb65fbef176e8357d4fcfeda394cdef9d98474c7
David Morais <davidmorais92@gmail.com>
2019-06-29 15:29:45 +0100

    Adjust color picker and label picker   - Make color picker a collapsible component

--- app/components/UI/ColorPicker.jsx
+++ app/components/UI/ColorPicker.jsx
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { TextInput, Box } from 'grommet';
+import { Collapsible, Box, Text } from 'grommet';
 import _ from 'lodash';
+import cx from 'classnames';
+import './ColorPicker.scss';
 
 const Input = (props) => {
   const {
     colors, value, onChange, label,
   } = props;
+  const [colorsExpanded, expandColors] = useState(false);
 
   return (
-    <Box className="color-options">
-      {colors.map(color => (
-        <svg
-          onClick={() => onChange(_.camelCase(label), color)}
-          value={value}
-          key={color}
-        >
-          <circle r="15" fill={color} />
-        </svg>
-      ))}
+    <Box justify="center" className={cx('color-options', colorsExpanded && 'options-expanded')}>
+      <Text onClick={() => expandColors(!colorsExpanded)}>
+        Select Color
+      </Text>
+      <Collapsible
+        open={colorsExpanded}
+        direction="vertical"
+      >
+        {colors.map(color => (
+          <svg
+            className={colorsExpanded ? 'options-expanded' : 'options-collapsed'}
+            onClick={() => {
+              expandColors(!colorsExpanded);
+              onChange(_.camelCase(label), color);
+            }}
+            value={value}
+            key={color}
+          >
+            <circle r="15" fill={color} />
+          </svg>
+        ))}
+      </Collapsible>
     </Box>
   );
 };
