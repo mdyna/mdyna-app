587b95eaf5dbcd04e54fc9b32d722f6252bf61b8
David Morais <davidmorais92@gmail.com>
2019-07-05 20:59:23 +0100

    Add suggestions to search bar

--- app/components/UI/Search.jsx
+++ app/components/UI/Search.jsx
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { TextInput } from 'grommet';
-import { Search } from 'grommet-icons';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { TextInput } from "grommet";
+import { Search } from "grommet-icons";
 
-import './Search.scss';
+import "./Search.scss";
 
 export default class SearchComponent extends Component {
   render() {
-    const {
-      searchInput, onChange, searchBar,
-    } = this.props;
+    const { searchInput, onChange, searchBar, titles } = this.props;
     return (
       <div className="search-wrapper">
         <Search color="brand" />
         <TextInput
+          suggestions={titles}
           className="mdyna-search"
           placeHolder="Search cards by title (Ctrl+P)"
           onChange={e => onChange(e.target.value)}
@@ -29,9 +28,9 @@ export default class SearchComponent extends Component {
 SearchComponent.propTypes = {
   searchInput: PropTypes.string,
   onChange: PropTypes.func.isRequired,
-  searchBar: PropTypes.object.isRequired,
+  searchBar: PropTypes.object.isRequired
 };
 
 SearchComponent.defaultProps = {
-  searchInput: '',
+  searchInput: ""
 };
