0622d1883e024a653c5e79e8b9e2d5787b34d6da
David Morais <davidmorais92@gmail.com>
2019-08-15 14:14:47 +0100

    Fix  changeTitle action

--- app/containers/CardItem.js
+++ app/containers/CardItem.js
@@ -6,13 +6,15 @@ const {
   CARD_EDITOR, CARD, LABEL, FILTERS,
 } = ACTIONS;
 
-const { editCard, changeCardSetting, changeTitle } = CARD_EDITOR;
+const { editCard, changeCardSetting } = CARD_EDITOR;
 
 const { addLabel, removeLabel } = LABEL;
 
 const { focusCard, addLabelFilter, removeLabelFilter } = FILTERS;
 
-const { removeCard, toggleCard, saveCard } = CARD;
+const {
+  removeCard, toggleCard, saveCard, changeTitle,
+} = CARD;
 
 function mapDispatchToProps(dispatch) {
   return {
