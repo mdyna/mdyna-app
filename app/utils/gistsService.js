d52c19e6e2fef238a80935fb3fcec68d5c25ce20
David Morais <davidmorais92@gmail.com>
2019-09-16 16:59:35 +0100

    Add getCurrentGist and  tweak sync functionality - Reset GH credentials if login fails

--- app/utils/gistsService.js
+++ app/utils/gistsService.js
@@ -1,5 +1,7 @@
 import Gists from 'gists';
 import Error from 'UI/Error';
+import unNest from 'Utils/nest';
+import uniqBy from 'lodash/uniqBy';
 import { getUserData } from 'Store';
 
 class GistsService {
@@ -9,6 +11,11 @@ class GistsService {
     this.gists = [];
     this.gistList = [];
     this.gistId = '';
+  }
+
+  updateGistId(id) {
+    this.gistId = id;
+    this.syncGist();
   }
 
   loginToGh(username, pw) {
@@ -36,6 +43,54 @@ class GistsService {
       Error.throwError(`Error getting user gists for ${this.username}`);
     }
     return [];
+  }
+
+  async getCurrentGist() {
+    const gist = await this.gists.get(this.gistId);
+    if (gist && gist.body && gist.body.files && gist.body.files['mdyna.json']) {
+      return JSON.parse(gist.body.files['mdyna.json'].content) || {};
+    }
+    return '';
+  }
+
+  async syncGist() {
+    try {
+      const currentGist = await this.getCurrentGist();
+      const currentUserData = getUserData();
+      const uniqUserCards = uniqBy(
+        [...currentGist.cards, ...currentUserData.cards],
+        'id',
+      );
+      const uniqUserLabels = uniqBy(
+        [...currentGist.labels, ...currentUserData.labels],
+        'title',
+      );
+      const uniqUserBoards = {
+        boardList: {
+          ...unNest(currentGist, 'boards.boardList'),
+          ...unNest(currentUserData, 'boards.boardList'),
+        },
+        boardNames: new Set([
+          ...unNest(currentUserData, 'boards.boardNames'),
+          ...unNest(currentGist, 'boards.boardNames'),
+        ]),
+      };
+      const content = {
+        cards: uniqUserCards,
+        boards: uniqUserBoards,
+        labels: uniqUserLabels,
+        lastSync: new Date(),
+      };
+      await this.gists.edit(this.gistId, {
+        files: {
+          'mdyna.json': JSON.stringify({
+            content,
+          }),
+        },
+      });
+    } catch {
+      Error.throwError('Could not sync with Gist');
+    }
   }
 
   async createGist() {
