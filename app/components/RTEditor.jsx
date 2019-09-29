91f1318fabeb1813c6deb22847160b5cbe7a34e8
David Morais <davidmorais92@gmail.com>
2019-09-29 18:18:14 +0100

    Create RTEditor component and themes

---
+++ app/components/RTEditor.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import cx from 'classnames';
+import Editor from 'rich-markdown-editor';
+import MarkdownSerializer from 'slate-md-serializer';
+import { light, colors } from './RTEditorThemes';
+
+const Markdown = new MarkdownSerializer();
+class RTEditor extends React.PureComponent {
+  editorRef = React.createRef();
+
+  componentDidUpdate(prevProps, prevState) {
+    const { value } = this.props;
+    if (value !== prevProps.value) {
+      this.editorRef.current.setState({
+        editorValue: Markdown.deserialize(value),
+      });
+    }
+  }
+
+  handleChange = (value) => {
+    const { onChange } = this.props;
+    if (onChange) {
+      onChange(value());
+    }
+  };
+
+  render() {
+    const {
+      value, readOnly, onSave, className, card,
+    } = this.props;
+    return (
+      <Editor
+        ref={this.editorRef}
+        className={cx(className, 'mdyna-md')}
+        readOnly={readOnly}
+        autoFocus={!readOnly}
+        defaultValue={value}
+        onSave={() => onSave(card)}
+        onChange={val => this.handleChange(val)}
+        onSearchLink={async (term) => {
+          console.log('Searched link: ', term);
+          return [
+            {
+              title: term,
+              url: 'localhost',
+            },
+          ];
+        }}
+        toc
+        theme={light}
+      />
+    );
+  }
+}
+
+export default RTEditor;
---
+++ app/components/RTEditorThemes.jsx
@@ -0,0 +1,70 @@
+export const colors = {
+  almostBlack: '#181f27',
+  lightBlack: '#2F3336',
+  almostWhite: '#E6E6E6',
+  white: '#1de9b6',
+  white10: 'rgba(255, 255, 255, 0.1)',
+  black: '#000',
+  black10: 'rgba(0, 0, 0, 0.1)',
+  primary: '#1AB6FF',
+  greyLight: '#F4F7FA',
+  grey: '#E8EBED',
+  greyMid: '#9BA6B2',
+  greyDark: '#DAE1E9',
+};
+
+export const base = {
+  ...colors,
+  fontFamily:
+    '\'Metric\', \'Segoe UI\',Roboto,Oxygen, Ubuntu,Cantarell,\'Open Sans\',\'Helvetica Neue\',sans-serif',
+  fontFamilyMono:
+    '\'SFMono-Regular\',Consolas,\'Liberation Mono\', Menlo, Courier,monospace',
+  fontWeight: 400,
+  fontSize: 18,
+  zIndex: 100,
+  link: colors.primary,
+  placeholder: '#B1BECC',
+  textSecondary: '#4E5C6E',
+  textLight: colors.white,
+  selected: colors.primary,
+  codeComment: '#6a737d',
+  codePunctuation: '#5e6687',
+  codeNumber: '#d73a49',
+  codeProperty: '#c08b30',
+  codeTag: '#3d8fd1',
+  codeString: '#032f62',
+  codeSelector: '#6679cc',
+  codeAttr: '#c76b29',
+  codeEntity: '#22a2c9',
+  codeKeyword: '#d73a49',
+  codeFunction: '#6f42c1',
+  codeStatement: '#22a2c9',
+  codePlaceholder: '#3d8fd1',
+  codeInserted: '#202746',
+  codeImportant: '#c94922',
+};
+export const light = {
+  ...base,
+  background: 'transparent',
+  text: colors.almostBlack,
+  code: colors.lightBlack,
+
+  toolbarBackground: colors.lightBlack,
+  toolbarInput: colors.white10,
+  toolbarItem: colors.white,
+
+  blockToolbarBackground: colors.almostBlack,
+  blockToolbarTrigger: colors.almostBlack,
+  blockToolbarTriggerIcon: colors.white,
+  blockToolbarItem: colors.white,
+
+  tableDivider: colors.grey,
+  tableSelected: colors.primary,
+  tableSelectedBackground: '#E5F7FF',
+
+  quote: colors.greyDark,
+  codeBackground: colors.greyLight,
+  codeBorder: colors.grey,
+  horizontalRule: colors.grey,
+  imageErrorBackground: colors.greyLight,
+};
