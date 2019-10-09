e091a468ca6d0a4d70d3bfee85b09cf81b143f13
David Morais <davidmorais92@gmail.com>
2019-10-09 23:31:21 +0100

    Update tooltips content to remove markdown   - Remove markdown shortcuts tooltip as the editor already offers markdown shortcuts

--- app/components/UI/tooltipsContent.js
+++ app/components/UI/tooltipsContent.js
@@ -1,41 +1,14 @@
 const tooltips = {
   keyboard: {
     title: 'Keyboard Shortcuts',
-    text: `
-- **A** - Add New Note
-- **Left Arrow** - Previous Cards
-- **Right Arrow** - Next Cards
-- **Ctrl+P** - Quick Search
-- **Ctrl+Enter** - Submit Note *while in editor*`,
-  },
-  markdown: {
-    title: 'Markdown Guide',
-    text: `
-## Titles
-- # Title
-- ## Sub-Title
-- ### Sub-sub-title ^^
-
-## Formatting
-- *Italic*
-- **bold**
-- ~~strikethrough~~
-
-## Lists
-- 1. Ordered List
-* * OR - Unordered List
-- - [ ] Task List
-
-## Other
-
-- \`inline code\`
-- Code Snipet
-\`\`\`javascript
-code snippet
-\`\`\`
-- > quote
-- [ Link Description ]( <URL> )
-    `,
+    text: [
+      '"A" to add a new note',
+      '"Left Arrow" to see previous cards',
+      '"Right Arrow" to see next cards',
+      '"Ctrl+P" to search',
+      '"Ctrl+Enter" to save your card while in the card editor',
+      '"Esc" brings back the card list when you focus a card',
+    ],
   },
 };
 
