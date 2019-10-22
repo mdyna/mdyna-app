4511b7f79835278fbca214fce34687ea172e607a
David Morais <davidmorais92@gmail.com>
2019-10-22 23:36:41 +0100

    Update focus card Icon

--- app/components/UI/FocusIcon.jsx
+++ app/components/UI/FocusIcon.jsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import Boards from 'Assets/icons/boards.svg';
+import Focus from 'Assets/icons/focus.svg';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import SVG from 'react-inlinesvg';
 import styled from 'styled-components';
 
 // eslint-disable-next-line react/prop-types
-const BoardsIcon = ({ className }) => (
-  <SVG className={className} src={Boards} />
-);
+const BoardsIcon = ({ className }) => <SVG className={className} src={Focus} />;
 
 const StyledBoardsIcon = styled(BoardsIcon)`
-  width: 24px;
+  width: 32px;
   vertical-align: bottom;
   margin: 5px;
   path {
     stroke-width: 12px;
-    fill: ${props => props.theme.global.colors.brand};
-    stroke: ${props => props.theme.global.colors.brand};
+    fill: ${props => props.color || props.theme.global.colors.brand};
+    stroke: ${props => props.color || props.theme.global.colors.brand};
   }
   rect {
-    stroke-width: 24px;
-    stroke: ${props => props.theme.global.colors.brand};
+    stroke-width: 40px;
+    stroke: ${props => props.color || props.theme.global.colors.brand};
   }
 `;
 
