3e8223b3ea66ff214f3e61bebe7f7c618dc60087
David Morais <davidmorais92@gmail.com>
2020-02-05 20:16:47 +0000

    Filter whyDidYouRender warnings

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { remote } from 'electron';
-import handleWindowControls from './utils/windowControls.js';
+import handleWindowControls from 'Utils/windowControls.js';
 import registerServiceWorker from './config/registerServiceWorker';
 import Root from './config/Root';
 import { Provider } from 'react-redux';
@@ -12,7 +12,7 @@ let MdynaStore = store;
 
 if (window.env === 'DEV') {
   const whyDidYouRender = require('@welldone-software/why-did-you-render');
-  whyDidYouRender(React);
+  whyDidYouRender(React, { exclude: [/^Switch/, /^Router/, /^Route/, /^Connect/] });
 }
 
 const render = Component => {
