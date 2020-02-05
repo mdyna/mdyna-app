38bead84ac514c8366da377b33cc57e3b0b58cef
David Morais <davidmorais92@gmail.com>
2020-02-05 20:34:27 +0000

    Add theme specific styling to scrollbars

--- app/utils/titlebarTheme.js
+++ app/utils/titlebarTheme.js
@@ -2,4 +2,21 @@ export default function ApplyTheme(theme) {
   const titleBarTitle = document.getElementById('titlebar');
   titleBarTitle.style.color = theme.text;
   titleBarTitle.style.background = theme.background;
+  const rootElement = document.getElementById('root');
+  rootElement.style.background = theme.background;
+  const css = `
+  ::-webkit-scrollbar-thumb  {
+    background: ${theme.brand}
+    }`;
+  const scrollbarStyles = document.getElementById('scrollbar-style');
+  if (scrollbarStyles) {
+    scrollbarStyles.innerHTML = css;
+  } else {
+    const head = document.head || document.getElementsByTagName('head')[0];
+    const style = document.createElement('style');
+    style.setAttribute('id', 'scrollbar-style');
+    head.appendChild(style);
+    style.type = 'text/css';
+    style.appendChild(document.createTextNode(css));
+  }
 }
