ce7357c2541525d1add121d740d9cf530432622c
David Morais <davidmorais92@gmail.com>
2020-02-09 23:37:29 +0000

    Refactor saveState to storage logic   - Switch from throtling to debounce     - Add debounce timer of 5 seconds     - App will only save to local storage if state doesn0t change for 5 seconds

--- app/store/index.js
+++ app/store/index.js
@@ -1,6 +1,6 @@
 import { applyMiddleware, createStore } from 'redux';
 import logger from 'redux-logger';
-import { throttle } from 'lodash'; // eslint-disable-line
+import { debounce } from 'lodash'; // eslint-disable-line
 import createDebounce from 'redux-debounced';
 import MdynaApp from './reducers';
 import { getLocalState, saveState } from './localStorage';
@@ -13,31 +13,35 @@ const store = createStore(
   applyMiddleware(logger, createDebounce()),
 );
 
+const saveStateToStorage = () => {
+  const {
+    cards, labels, boards, favs, settings, style, filters,
+  } = store.getState();
+  saveState(
+    {
+      cards,
+      labels,
+      boards,
+      favs,
+    },
+    {
+      codeTheme: settings.codeTheme,
+      theme: style.theme,
+      githubUserName: settings.githubUserName,
+      gistId: settings.gistId,
+      githubPassword: settings.githubPassword,
+      order: filters.order,
+      activeBoard: filters.activeBoard,
+      cardsPerPage: settings.cardsPerPage,
+      deletedCards: settings.deletedCards || [],
+      sorting: filters.sorting,
+      cwd: settings.cwd,
+    },
+  );
+};
+
 store.subscribe(
-  throttle(() => {
-    saveState(
-      {
-        cards: store.getState().cards,
-        labels: store.getState().labels,
-        boards: store.getState().boards,
-        favs: store.getState().favs,
-      },
-      {
-        codeTheme: store.getState().settings.codeTheme,
-        theme: store.getState().style.theme,
-        githubUserName: store.getState().settings.githubUserName,
-        gistId: store.getState().settings.gistId,
-        githubPassword: store.getState().settings.githubPassword,
-        order: store.getState().filters.order,
-        activeBoard: store.getState().filters.activeBoard,
-        cardsPerPage: store.getState().settings.cardsPerPage,
-        deletedCards: store.getState().settings.deletedCards || [],
-        sorting: store.getState().filters.sorting,
-        cwd: store.getState().settings.cwd,
-      },
-    );
-  }),
-  1000,
+  debounce(saveStateToStorage, 5000),
 );
 
 export const getUserData = () => ({
