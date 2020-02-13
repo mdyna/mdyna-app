d028387877372b600d4b66d737976f3efc3c6f9b
David Morais <davidmorais92@gmail.com>
2020-02-13 19:41:39 +0000

    Add template picker component

---
+++ app/components/UI/TemplatePicker.jsx
@@ -0,0 +1,41 @@
+import React, { PureComponent } from 'react';
+import { Menu } from 'grommet';
+import { Down } from 'grommet-icons';
+import PropTypes from 'prop-types';
+
+
+import TEMPLATES from './MarkdownTemplates';
+
+export default class TemplatePicker extends PureComponent {
+  render() {
+    const {
+      onClick,
+    } = this.props;
+    const templateKeys = Object.keys(TEMPLATES);
+    console.log();
+    return (
+      <Menu
+        justifyContent="center"
+        className="boards-menu"
+        dropBackground="dark-2"
+        icon={<Down color="brand" />}
+        dropAlign={{ top: 'bottom' }}
+        items={[
+          ...templateKeys.map(
+            (key) => {
+              const template = TEMPLATES[key];
+              return {
+                label: template.title,
+                onClick: () => onClick(template.text),
+              };
+            },
+          ),
+        ]}
+      />
+    );
+  }
+}
+
+TemplatePicker.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
